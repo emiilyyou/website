eac1be0b9a18b587c012068953077944080831ac
Emily You <160804982+emiilyyou@users.noreply.github.com>
2025-01-17 22:41:47 -0500

    deploy part 1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/website/',
   plugins: [react()],
+  // Inject <base> tag dynamically
+  plugins: [
+    {
+      name: 'inject-base-tag',
+      transformIndexHtml(html) {
+        // Inject the <base> tag
+        return html.replace(
+          /<head>/,
+          `<head><base href="/website/" />` // Adjust this to match your GitHub Pages repo name
+        );
+      },
+    },
+  ],
   css: {
     modules: {
       localsConvention: "camelCase",
